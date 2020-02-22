e2422e17b4c0eebfb4db4cc91e4c06cfc77ceffd
raviteja <madishettyraviteja2011@gmail.com>
2020-02-22 12:47:14 +0000

    Employee project

---
+++ Employee/src/main/java/com/assessment/Employee/EmployeeApplication.java
@@ -0,0 +1,17 @@
+package com.assessment.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Helloworld");
+        SpringApplication.run(EmployeeApplication.class, args);
+
+
+    }
+
+}
